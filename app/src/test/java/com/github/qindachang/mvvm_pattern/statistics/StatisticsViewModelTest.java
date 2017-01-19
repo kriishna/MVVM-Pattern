18ff92121973d40433b0eda7392024681b5879a0
qin <823951895@qq.com>
2017-01-20 01:00:43 +0800

    add test

---
+++ app/src/test/java/com/github/qindachang/mvvm_pattern/statistics/StatisticsViewModelTest.java
@@ -0,0 +1,22 @@
+package com.github.qindachang.mvvm_pattern.statistics;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by qin on 2017/1/20.
+ */
+public class StatisticsViewModelTest {
+
+    @Test
+    public void onButtonAddClick() throws Exception {
+
+    }
+
+    @Test
+    public void onButtonControlClick() throws Exception {
+
+    }
+
+}
